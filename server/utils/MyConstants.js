35e94c6eca2f0f8e048dadd92ec1f3c461517dca
Kiet2k2 <kiet.207ct68645@vanlanguni.vn>
2023-07-12 13:41:28 +0700

    shoppingonline

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shoppingonline.m0oxtn4.mongodb.net',
+    DB_USER: 'minh207ct68653',
+    DB_PASS: 'Toidangcodon1',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'congminh0801@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'gzgpqqeiwqzvoefs',
+    JWT_SECRET: '<jwt_secret>',
+    JWT_EXPIRES: '60000000', // in milliseconds
+  };
+  module.exports = MyConstants;
